2d5d8605f7e9b9e6be63aa4a070431acc92ffd3d
Marquinhos <59203956+Stokomp@users.noreply.github.com>
2023-02-21 17:51:55 -0300

    Add files via upload

---
+++ Arquivos/Intune/Endpoint Analytics/Exemplos/Remediate_Anki_Winget.ps1
@@ -0,0 +1,7 @@
+#Upgrade via Winget
+# Acesse o site https://winget.run/ e no campo de busca coloque o nome do software. Atraves deste site voce sera capaz de encontrar
+# todos os comandos de upgrade baseado em Winget.
+
+#Inicio do script
+winget install -e --id Anki.Anki -h
+Exit 0
